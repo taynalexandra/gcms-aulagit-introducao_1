e2365c519bf0ca5bb51dcb3d02363b25bac9ad06
taynalexandra <tayna_alexandra@outlook.com>
2020-09-27 21:46:11 -0300

    4º Commit BASH - Introdução GIT

--- src/gcms/aulagit/introducao/GCMSAulaGitIntroducao.java
+++ src/gcms/aulagit/introducao/GCMSAulaGitIntroducao.java
@@ -10,7 +10,7 @@ public class GCMSAulaGitIntroducao {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Aula 1 - Introdução GIT");
+        System.out.println("Aula 1 - Introdução GIT - BASH");
     }
     
 }
