1eaa11964b34d49ff0614e1a2d4afd4ff087997a
taynalexandra <tayna_alexandra@outlook.com>
2020-09-27 21:02:34 -0300

    2º Commit Netbeans - Introdução GIT

--- src/gcms/aulagit/introducao/GCMSAulaGitIntroducao.java
+++ src/gcms/aulagit/introducao/GCMSAulaGitIntroducao.java
@@ -10,7 +10,7 @@ public class GCMSAulaGitIntroducao {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Teste");
+        System.out.println("Aula 1 - Introdução GIT");
     }
     
 }
